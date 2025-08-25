9e2bca863e9bb97b24b2ffe7f698bb03b4f58b39
1xBee <184531595+1xBee@users.noreply.github.com>
2025-08-25 14:52:34 +0000

    changed firebase config to object with env variabls

--- db.js
+++ db.js
@@ -6,11 +6,19 @@ import admin from 'firebase-admin'
 // 1. Point to your downloaded service account key file
 //    Replace './serviceAccountKey.json' with the actual path to your file.
 // const serviceAccount = require('./privet_folder/project-1-firestore-firebase-adminsdk-fbsvc-fe4ae240fb.json');
-import serviceAccount from './config/project-1-firestore-firebase-adminsdk-fbsvc-fe4ae240fb.json' with { type: 'json' };
+// import serviceAccount from './config/project-1-firestore-firebase-adminsdk-fbsvc-fe4ae240fb.json' with { type: 'json' };
 
 // 2. Initialize the Firebase Admin SDK
+
+let privateKey = process.env.firebse_private_key;
+privateKey = privateKey.replace(/\\n/g, '\n');
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.firebase_project_id,
+    clientEmail: process.env.firebase_client_email,
+    privateKey: privateKey
+  }),
   // 3. Specify your Firebase Project ID (replace 'your-project-id' with 'project-1-firestore')
   projectId: 'project-1-firestore'
 });
